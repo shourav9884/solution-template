087469ff19273d340bff7729c501a55e45ae3b2f
shourav9884 <shourav9884@gmail.com>
2017-05-31 22:22:01 +0600

    reformatted code

--- src/main/java/com/tigerit/exam/Solution.java
+++ src/main/java/com/tigerit/exam/Solution.java
@@ -25,11 +25,10 @@ public class Solution implements Runnable {
         // sample input process
 //        String string = readLine();
         Integer numOfTestCase = readLineAsInteger();
-        for(int k=0;k<numOfTestCase;k++)
-        {
+        for (int k = 0; k < numOfTestCase; k++) {
             Integer numOfTables = readLineAsInteger();
             List<Table> tables = new ArrayList<>();
-            for(int i = 0 ;i<numOfTables;i++){
+            for (int i = 0; i < numOfTables; i++) {
                 Table table = getTableFromInput();
                 tables.add(table);
             }
@@ -40,149 +39,135 @@ public class Solution implements Runnable {
                 queries.add(getSingleQueryFromInput());
                 readLine();
             }
-            printLine("Test: "+(k+1));
-            printResult(tables,queries);
+            printLine("Test: " + (k + 1));
+            printResult(tables, queries);
         }
     }
 
     private void printResult(List<Table> tables, List<Query> queries) {
-        for (int i = 0; i < queries.size(); i++) {
-            printSingleQueryResult(tables, queries.get(i));
+        for (Query query:queries) {
+            printSingleQueryResult(tables, query);
             printLine("");
         }
     }
 
     private void printSingleQueryResult(List<Table> tables, Query query) {
-        Map<String,Table> tableMap = new HashMap<>();
-        for(Table table:tables) tableMap.put(table.name,table);
+        Map<String, Table> tableMap = new HashMap<>();
+        for (Table table : tables) tableMap.put(table.name, table);
         Table resultTable = new Table();
         List<String> resultColumns = new ArrayList<>();
 
         Table fromTable = tableMap.get(query.fromTable);
         Table joinTable = tableMap.get(query.joinTable);
 
-        if(query.selectColumns.equals("*")){
+        if (query.selectColumns.equals("*")) {
 
-
-            for(int i=0;i<fromTable.columns.size();i++){
+            for (int i = 0; i < fromTable.columns.size(); i++) {
                 resultColumns.add(fromTable.columns.get(i));
             }
-            for(int i=0;i<joinTable.columns.size();i++){
+            for (int i = 0; i < joinTable.columns.size(); i++) {
                 resultColumns.add(joinTable.columns.get(i));
             }
             resultTable.columns = resultColumns;
 
-        }
-        else
-        {
+        } else {
             String[] cols = query.selectColumns.split(",");
 
-            for (int i=0;i<cols.length;i++){
+            for (int i = 0; i < cols.length; i++) {
                 String column = cols[i].trim();
                 String[] arr = column.split("\\.");
-                if(arr.length>1)resultColumns.add(arr[1].trim());
+                if (arr.length > 1) resultColumns.add(arr[1].trim());
                 else resultColumns.add(column);
-
             }
             resultTable.columns = resultColumns;
         }
         String onClause = query.onClause;
         String[] onClauseArr = onClause.split(" = ");
 
-        String[] ts = onClauseArr[0].split("\\.");
         String firstTable = onClauseArr[0].split("\\.")[0];
         String firstColumn = onClauseArr[0].split("\\.")[1];
         String secondTable = onClauseArr[1].split("\\.")[0];
         String secondColumn = onClauseArr[1].split("\\.")[1];
         String fromColumn = "";
         String onColumn = "";
-        if(firstTable.equals(query.fromTable) || firstTable.equals(query.fromTableShort)) {
+        if (firstTable.equals(query.fromTable) || firstTable.equals(query.fromTableShort)) {
             firstTable = query.fromTable;
             fromColumn = firstColumn;
-        }
-        else if(firstTable.equals(query.joinTable) || firstTable.equals(query.joinTableShort)) {
+        } else if (firstTable.equals(query.joinTable) || firstTable.equals(query.joinTableShort)) {
             firstTable = query.joinTable;
             onColumn = firstColumn;
         }
-        if(secondTable.equals(query.fromTable) || secondTable.equals(query.fromTableShort)) {
+        if (secondTable.equals(query.fromTable) || secondTable.equals(query.fromTableShort)) {
             secondTable = query.fromTable;
             fromColumn = secondColumn;
-        }
-        else if(secondTable.equals(query.joinTable) || secondTable.equals(query.joinTableShort)) {
+        } else if (secondTable.equals(query.joinTable) || secondTable.equals(query.joinTableShort)) {
             secondTable = query.joinTable;
             onColumn = secondColumn;
         }
 
         List<List<Integer>> resultRows = new ArrayList<>();
-        for(int i=0;i<fromTable.rows.size();i++){
-            for(int j=0;j<joinTable.rows.size();j++){
-                if(fromTable.rows.get(i).get(fromTable.columnIndexMap.get(fromColumn))==joinTable.rows.get(j).get(joinTable.columnIndexMap.get(onColumn)))
-                {
-                    //concat
-                    resultRows.add(addRow(fromTable,joinTable,query,fromTable.rows.get(i),joinTable.rows.get(j)));
+        for (int i = 0; i < fromTable.rows.size(); i++) {
+            for (int j = 0; j < joinTable.rows.size(); j++) {
+                if (fromTable.rows.get(i).get(fromTable.columnIndexMap.get(fromColumn)) == joinTable.rows.get(j).get(joinTable.columnIndexMap.get(onColumn))) {
+                    resultRows.add(addRow(fromTable, joinTable, query, fromTable.rows.get(i), joinTable.rows.get(j)));
                 }
             }
         }
 
         resultTable.rows = resultRows;
         printTable(resultTable);
-//        printLine(query.toString());
     }
 
     private void printTable(Table resultTable) {
         String column = "";
-        for(int i=0;i<resultTable.columns.size();i++){
-            column+=resultTable.columns.get(i)+" ";
+        for (int i = 0; i < resultTable.columns.size(); i++) {
+            column += resultTable.columns.get(i) + " ";
         }
         printLine(column.trim());
-        for(int i=0;i<resultTable.rows.size();i++){
+        for (int i = 0; i < resultTable.rows.size(); i++) {
             String row = "";
-            for(int j=0;j<resultTable.rows.get(i).size();j++){
-                row+=resultTable.rows.get(i).get(j)+" ";
+            for (int j = 0; j < resultTable.rows.get(i).size(); j++) {
+                row += resultTable.rows.get(i).get(j) + " ";
             }
             printLine(row.trim());
         }
     }
 
-    private List<Integer> addRow(Table fromTable,Table joinTable,Query query, List<Integer> fromRow, List<Integer> joinRow){
+    private List<Integer> addRow(Table fromTable, Table joinTable, Query query, List<Integer> fromRow, List<Integer> joinRow) {
         String columns = query.selectColumns;
         List<Integer> result = new ArrayList<>();
-        if(columns.equals("*")){
-            for(int i=0;i<fromRow.size();i++)
-            {
+        if (columns.equals("*")) {
+            for (int i = 0; i < fromRow.size(); i++) {
                 result.add(fromRow.get(i));
             }
-            for(int i=0;i<joinRow.size();i++)
-            {
+            for (int i = 0; i < joinRow.size(); i++) {
                 result.add(joinRow.get(i));
             }
-        }
-        else{
+        } else {
             String[] selectedColumns = columns.split(",");
-            Map<Integer,Boolean> fromColumns = new HashMap<>();
-            Map<Integer,Boolean> joinColumns = new HashMap<>();
+            Map<Integer, Boolean> fromColumns = new HashMap<>();
+            Map<Integer, Boolean> joinColumns = new HashMap<>();
 
-            for(int i=0;i<selectedColumns.length;i++){
+            for (int i = 0; i < selectedColumns.length; i++) {
                 String column = selectedColumns[i].trim();
                 String tableName = column.split("\\.")[0];
                 String columnName = column.split("\\.")[1];
-                if(query.fromTableShort.equals(tableName)|| query.fromTable.equals(tableName))
-                    fromColumns.put(fromTable.columnIndexMap.get(columnName),true);
+                if (query.fromTableShort.equals(tableName) || query.fromTable.equals(tableName))
+                    fromColumns.put(fromTable.columnIndexMap.get(columnName), true);
                 else
-                    joinColumns.put(joinTable.columnIndexMap.get(columnName),true);
+                    joinColumns.put(joinTable.columnIndexMap.get(columnName), true);
             }
-            for(int i=0;i<fromRow.size();i++)
-            {
-                if(fromColumns.containsKey(i)) result.add(fromRow.get(i));
+            for (int i = 0; i < fromRow.size(); i++) {
+                if (fromColumns.containsKey(i)) result.add(fromRow.get(i));
             }
-            for(int i=0;i<joinRow.size();i++)
-            {
-                if(joinColumns.containsKey(i)) result.add(joinRow.get(i));
+            for (int i = 0; i < joinRow.size(); i++) {
+                if (joinColumns.containsKey(i)) result.add(joinRow.get(i));
             }
         }
         return result;
     }
-    private Query getSingleQueryFromInput(){
+
+    private Query getSingleQueryFromInput() {
         Query query = new Query();
         query = getSelectColumns(query);
         query = getFromTable(query);
@@ -190,40 +175,40 @@ public class Solution implements Runnable {
         query = getOnClauseString(query);
         return query;
     }
+
     private Query getOnClauseString(Query query) {
         String onClauseString = readLine();
-        onClauseString = onClauseString.replace("ON ","").trim();
+        onClauseString = onClauseString.replace("ON ", "").trim();
         query.onClause = onClauseString;
         return query;
     }
 
     private Query getJoinTable(Query query) {
         String joinTableString = readLine();
-        joinTableString = joinTableString.replace("JOIN ","").trim();
+        joinTableString = joinTableString.replace("JOIN ", "").trim();
         String[] joinTableArr = joinTableString.split(" ");
         query.joinTable = joinTableArr[0];
-        if(joinTableArr.length>1) query.joinTableShort = joinTableArr[1];
+        if (joinTableArr.length > 1) query.joinTableShort = joinTableArr[1];
         return query;
     }
 
     private Query getFromTable(Query query) {
         String fromTableString = readLine();
-        fromTableString = fromTableString.replace("FROM ","").trim();
+        fromTableString = fromTableString.replace("FROM ", "").trim();
         String[] fromTableArr = fromTableString.split(" ");
         query.fromTable = fromTableArr[0];
-        if(fromTableArr.length>1) query.fromTableShort = fromTableArr[1];
+        if (fromTableArr.length > 1) query.fromTableShort = fromTableArr[1];
         return query;
     }
 
     private Query getSelectColumns(Query query) {
         String selectString = readLine();
-        selectString = selectString.replace("SELECT ","").trim();
+        selectString = selectString.replace("SELECT ", "").trim();
         query.selectColumns = selectString;
         return query;
     }
 
-    private Table getTableFromInput()
-    {
+    private Table getTableFromInput() {
         Table table = new Table();
         table.name = readLine();
 
@@ -235,15 +220,15 @@ public class Solution implements Runnable {
 
 
         table.columns = getColumns(columnCount);
-        Map<String,Integer> columnMap = new HashMap<>();
-        for(int i=0;i<table.columns.size();i++){
-            columnMap.put(table.columns.get(i),i);
+        Map<String, Integer> columnMap = new HashMap<>();
+        for (int i = 0; i < table.columns.size(); i++) {
+            columnMap.put(table.columns.get(i), i);
         }
         table.columnIndexMap = columnMap;
 
         List<List<Integer>> rows = new ArrayList<>();
 
-        for(int j=0;j<rowCount;j++){
+        for (int j = 0; j < rowCount; j++) {
             List<Integer> row = getSingleRow(columnCount);
             rows.add(row);
         }
@@ -251,6 +236,7 @@ public class Solution implements Runnable {
         table.rows = rows;
         return table;
     }
+
     private List<String> getColumns(int columnCount) {
         String columnInLine = readLine();
         String[] splittedColumns = columnInLine.split(" ");
@@ -267,7 +253,7 @@ public class Solution implements Runnable {
         List<Integer> row = new ArrayList<>();
         String rowLine = readLine();
         String[] rowStrings = rowLine.split(" ");
-        for(int i=0;i<columnCount;i++) row.add(Integer.parseInt(rowStrings[i]));
+        for (int i = 0; i < columnCount; i++) row.add(Integer.parseInt(rowStrings[i]));
         return row;
     }
 }
